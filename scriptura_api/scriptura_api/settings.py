c282bf5521fc7add06e5546ef070bb97148ad78f
Cruizzer <archcruizer@gmail.com>
2026-02-25 10:25:57 +0000

    Create and register apps

--- scriptura_api/scriptura_api/settings.py
+++ scriptura_api/scriptura_api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'core',
+    'analytics',
+    'themes',
+    'ingestion',
 ]
 
 MIDDLEWARE = [
