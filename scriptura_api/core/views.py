ef3837a8645555b050b5c4e2312e57cd1744a17f
Cruizzer <archcruizer@gmail.com>
2026-02-25 11:01:36 +0000

    Added a Book analytics view

--- scriptura_api/core/views.py
+++ scriptura_api/core/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+
+class BookAnalyticsView(APIView):
+    def get(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        
+        # Aggregate all verse texts in the book
+        text = " ".join(v.text for c in book.chapters.all() for v in c.verses.all())
+        word_count = len(text.split())
+        return Response({
+            "book": book.name,
+            "word_count": word_count
+        })
 
 class BookViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Book.objects.all().order_by('order')
